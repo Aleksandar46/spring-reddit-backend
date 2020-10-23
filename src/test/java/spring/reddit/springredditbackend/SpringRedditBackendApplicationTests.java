8a8b3a5214c2c11d61cb5215c0bd1b5bda799e79
Aleksandar46 <aleksandarilievski62@gmail.com>
2020-10-24 00:43:51 +0200

    Initial commit

---
+++ src/test/java/spring/reddit/springredditbackend/SpringRedditBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.reddit.springredditbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedditBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
